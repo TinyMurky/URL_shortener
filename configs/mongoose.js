0a77bc41a749a2789fc71425f4b6e29b5f095b0b
TinyMurky <murky0830@gmail.com>
2023-05-13 20:51:52 +0800

    MongoDB connection and folder create

---
+++ configs/mongoose.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose")
+
+if (process.env.NODE_ENV !== "Production") {
+  require("dotenv").config({ path: ".env" })
+}
+
+const MONGODB_URL = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}/${process.env.MONGODB_REPO}?retryWrites=true&w=majority`
+mongoose.connect(MONGODB_URL)
+const db = mongoose.connection
+
+db.on("error", (error) => {
+  console.log("MongoDB connect error:")
+  console.error(error)
+})
+
+db.once("open", () => {
+  console.log("MongoDB connect correctly")
+})
+
+module.exports = db
